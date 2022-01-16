ef7d8a030734e23df3cf5e8f150404d92f2fa0da
Natália Lira <natilira@gmail.com>
2022-01-16 17:56:42 -0300

    Final aula 1

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 34;
+console.log(idade); 
+console.log(idade +2);
+console.log(idade -2);
+console.log(idade /2);
+console.log(idade *2);
+ 
+const idadeSomada = idade+2;
+console.log(idadeSomada);  // Boas Praticas variáveis camelCase
+
