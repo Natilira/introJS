44aabd0a157eb6b7278e6915cccf3c9f525f50d2
Natália Lira <natilira@gmail.com>
2022-01-17 10:28:23 -0300

    q+ https://github.com/Natilira/introJS
    "modificando aula 2"

--- 5-atribuicaoDeVariavel.js
+++ 5-atribuicaoDeVariavel.js
@@ -1,17 +1,17 @@
 console.log("Trabalhando com atribuição de variáveis");
 
 const idade = 34;
-let nome = "Natália";
+let primeiroNome = "Natália";
 const sobrenome = "Lira";
 
-console.log(nome + sobrenome); //desta forma sai junto
-console.log(nome + " " + sobrenome); // Forma 1 com espaço
-console.log(nome , sobrenome); // Forma 2 com espaço
+console.log(primeiroNome + sobrenome); //desta forma sai junto
+console.log(primeiroNome + " " + sobrenome); // Forma 1 com espaço
+console.log(primeiroNome , sobrenome); // Forma 2 com espaço
 
 // Outra forma de trabalhar com texto strings declaração com crase ``
 // interpolação de variável acontece quando usamos a crase entre elas ${}
-console.log(`Meu nome é ${nome} ${sobrenome}`);
-console.log(`Meu nome é ${nome + sobrenome}`); // a virgula não funciona neste caso
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
+console.log(`Meu nome é ${primeiroNome + sobrenome}`); // a virgula não funciona neste caso
 
 // sobre escrevendo a variável , não funciona se a variável anterior era uma const (constante)
 // let --> variável comum que pode variar ou mudar
